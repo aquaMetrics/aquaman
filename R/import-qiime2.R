9067b5651dd3debd45f23bbfbbf8432a1e67e626
Foster <Tim.Foster@SEPA.org.uk>
2024-03-08 11:46:10 +0000

    feat: export import_qiime2 function and document

--- R/import-qiime2.R
+++ R/import-qiime2.R
@@ -1,33 +1,16 @@
-
-
+#' Import taxon data from Qiime2
+#'
+#' Import .csv taxonomy file export from Qiime2 Viewer. The taxonomic data is
+#' re-structured to match the format required for the IQI model.
+#' @param path File path to the Qiime2 Viewer exported .csv file
+#'
+#' @return dataframe 682 columns with sample names as row.names.
+#' @export
+#'
+#' @examples
+#' data <- import_qiime2(system.file("extdat/raw-taxa-data", "S16_TestMOWI.csv", package = "aquaman"))
 import_qiime2 <- function(path = NULL) {
-
-
-  library(reshape2)
-  library(data.table)
-  library(splitstackshape)
-  library(stringr)
-  library(tidyverse)
-  library(vegan)
-
-
-  # import_file <- function(path) {
-  #   message("importing file")
-  #   # ID files to be uploaded and merged.
-  #   # Upload and combine files, fill across all taxa
-  #   data <- rbindlist(lapply(path, fread), fill = TRUE)
-  #   data <- as.data.frame(data)
-  #   data[is.na(data)] <- 0
-  #   data <-dplyr::select(data, -file_name, -RUN, -station, -Transect, -replicate)
-  #   message("Function: 'Upload16S' end")
-  #   return(data)
-  # }
-  #
-  # data <- import_file(path)
- #  C\Users\Tim.Foster\Documents\Projects\aquaman\inst\extdat\raw-taxa-data
- S16Data <- readr::read_csv(path)
-
-
+  S16Data <- readr::read_csv(path)
   SplitToTaxa <- function(S16Data) {
     message("SplitToTaxa start")
     message("Splits data into taxa levels")
@@ -39,11 +22,15 @@ import_qiime2 <- function(path = NULL) {
     # colnames(Run2.1)[grep("Unassigned",colnames(Run2.1))]="D_0Unassigned"
     Run2.2 <- as.data.frame(t(Run2.1[, -1])) # transpose, don't include the sample ID, need to keep the SampleID as colnames
     # Run2.2 <- setNames(data.frame(t(Run2.1[, -1])), Run2.1[, 1]) # setNames, make the colnames=SampleID
-    names(Run2.2) <-  unlist(Run2.1[, 1])
+    names(Run2.2) <- unlist(Run2.1[, 1])
     Run2.2$Taxa <- rownames(Run2.2)
     Run2.3 <- as.data.frame(Run2.2[, "Taxa"]) # isolate the taxa names, ahead of cleaning them up.
     colnames(Run2.3)[1] <- "Taxa"
-    Run2.4 <- cSplit(Run2.3, splitCols = "Taxa", sep = c("D_"), drop = FALSE) # D__is the divider between taxa, from Silva
+    Run2.4 <- splitstackshape::cSplit(Run2.3,
+      splitCols = "Taxa",
+      sep = c("D_"),
+      drop = FALSE
+    ) # D__is the divider between taxa, from Silva
     # the cSplit inserts an NA column because the split criterion prefixes the name to be split.
     Run2.4 <- Run2.4[, -2] # delete 1st unattributed column
     colnames(Run2.4)[2:8] <- c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")
@@ -60,7 +47,7 @@ import_qiime2 <- function(path = NULL) {
     Run2.4[, 2:8] <- lapply(Run2.4[, 2:8], gsub, pattern = "unidentified", replacement = NA, ignore.case = TRUE) #
     Run2.4[, 2:8] <- lapply(Run2.4[, 2:8], gsub, pattern = ";_", replacement = NA, ignore.case = TRUE) #
     Run2.4[, 2:8] <- lapply(Run2.4[, 2:8], gsub, pattern = "[^[:alnum:]]", replacement = "", ignore.case = TRUE) #
-    Run2.4[, 2:8] <- lapply(Run2.4[, 2:8], function(x) str_replace(x, ";", "")) #
+    Run2.4[, 2:8] <- lapply(Run2.4[, 2:8], function(x) stringr::str_replace(x, ";", "")) #
     Run2.4[, 2:8] <- lapply(Run2.4[, 2:8], trimws, which = "both") # removes leading and tailing white_spaces
     # the Run2.4[] -square brackets[] keep this as a dataframe.
     # Run2.4$Taxa=Run2.4a$Taxa
@@ -96,7 +83,7 @@ import_qiime2 <- function(path = NULL) {
 
   taxa <- SplitToTaxa(S16Data)
 
-  TaxaCollate <- function(MDS1 , taxalevel) {
+  TaxaCollate <- function(MDS1, taxalevel) {
     message("Calling function Taxacollate")
     message("Collates data across specified taxonomic level")
     # df=as.data.table(TestNGS2);taxalevel=TaxaLevel
@@ -105,14 +92,14 @@ import_qiime2 <- function(path = NULL) {
     Excludes2 <- setdiff(Excludes, taxalevel)
     df2 <- dplyr::select(df, -Excludes2) # df2 in data.table format hence  ..! works
     names(df2)[1] <- "Taxon"
-    df2 <- as.data.table(df2)
+    df2 <- data.table::as.data.table(df2)
     # df2 contains only the Taxon and the sample data.
-    df3 <- melt(df2, id = "Taxon") # rm(df3)
-    df4 <- data.table(df3)
+    df3 <- reshape2::melt(df2, id = "Taxon") # rm(df3)
+    df4 <- data.table::data.table(df3)
     names(df4) <- c("Taxon", "SampleID", "Total") # sensible names for summarisation step via data.table
     df4$Taxon <- as.factor(df4$Taxon)
     # browser()
-     df4$Total <- as.numeric(df4$Total)
+    df4$Total <- as.numeric(df4$Total)
     # setDT(df4)
     # df5 <- df4[,  # this generates the summary values by taxonomic group.
     #    sum(Total)
@@ -129,18 +116,18 @@ import_qiime2 <- function(path = NULL) {
 
   family <- TaxaCollate(taxa, "Family")
   family <- ungroup(family)
-  input <- dcast(family, SampleID ~ Taxon, value.var = "Total")
+  input <- reshape2::dcast(family, SampleID ~ Taxon, value.var = "Total")
 
   input[is.na(input)] <- 0
-  input_rare <- cbind(input[, 1], vegan:::rrarefy(input[, -1], 2500))
+  input_rare <- cbind(input[, 1], vegan::rrarefy(input[, -1], 2500))
   input_rare <- data.frame(input_rare)
   # input_test <- input_rare[, names(input_rare)[names(input_rare) %in% names(aquaman::demo_taxa)]]
   # input_test <- input_rare[, c(names(input_test))]
 
 
   input_rare <- dplyr::bind_rows(aquaman::demo_taxa, input_rare)
-  input_rare <- input_rare[(nrow(aquaman::demo_taxa) +1):nrow(input_rare), ]
+  input_rare <- input_rare[(nrow(aquaman::demo_taxa) + 1):nrow(input_rare), ]
   input_rare[is.na(input_rare)] <- 0
-  row.names(input_rare)  <- input[, 1]
+  row.names(input_rare) <- input[, 1]
   return(input_rare)
 }
